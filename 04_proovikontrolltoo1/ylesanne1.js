a4a74cc97aaf8d0fe14ca0f0dbd73cecda19ffa1
Mattias Tamm <tammmat@tlu.ee>
2025-03-05 17:01:28 +0000

    Kaks punkti esimesest ülesandest tehtud.

--- 04_proovikontrolltoo1/ylesanne1.js
+++ 04_proovikontrolltoo1/ylesanne1.js
@@ -8,9 +8,11 @@ Hulknurk
 * Lisa klassile käsklused kogu kujundi nihutamiseks ning suurendamiseks/vähendamiseks. Võimalda küsida punktide uued asukohad ning külgede pikkused, näita kujundit ekraanil.
 */
 var Hulknurk = /** @class */ (function () {
-    function Hulknurk(x, y) {
+    function Hulknurk(ctx, x, y) {
+        this.ctx = ctx;
         this.x = x;
         this.y = y;
+        this.draw();
     }
     Hulknurk.prototype.kuva = function () {
         console.log("Hulknurga nurkade koordinaadid on:");
@@ -22,13 +24,30 @@ var Hulknurk = /** @class */ (function () {
         var ymbermoot = 0;
         this.x.push(x);
         this.y.push(y);
-        for (var i = 0; i < this.x.length; i++) {
+        for (var i = 0; i < this.x.length - 1; i++) {
             ymbermoot += Math.sqrt(Math.pow(this.x[i + 1] - this.x[i], 2) + Math.pow(this.y[i + 1] - this.y[i], 2));
         }
+        ymbermoot += Math.sqrt(Math.pow(this.x[0] - this.x[this.x.length - 1], 2) + Math.pow(this.y[0] - this.y[this.y.length - 1], 2));
         return ymbermoot;
+    };
+    Hulknurk.prototype.draw = function () {
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x[0], this.y[0]);
+        for (var i = 1; i < this.x.length; i++) {
+            this.ctx.lineTo(this.x[i], this.y[i]);
+        }
+        this.ctx.lineTo(this.x[0], this.y[0]);
+        this.ctx;
+        this.ctx.stroke();
     };
     return Hulknurk;
 }());
-var hulknurk1 = new Hulknurk([0, 1, 1, 0], [0, 0, 1, 1]);
-var hulknurk2 = new Hulknurk([2, 4, 4, 2], [0, 0, 5, 5]);
-hulknurk1.lisaPunkt(3, 4);
+var canvas, ctx, hulknurk1, hulknurk2;
+window.onload = function () {
+    canvas = document.querySelector('#canvas1');
+    ctx = canvas.getContext('2d');
+    hulknurk1 = new Hulknurk(ctx, [100, 200, 200], [100, 100, 200]);
+    hulknurk1.draw();
+    hulknurk1.lisaPunkt(150, 150);
+    hulknurk1.kuva();
+};
--- 04_proovikontrolltoo1/ylesanne1.ts
+++ 04_proovikontrolltoo1/ylesanne1.ts
@@ -11,9 +11,10 @@ Hulknurk
 class Hulknurk {
     x: number[];
     y: number[];
-    constructor(x: number[], y: number[]){
+    constructor(protected ctx, x: number[], y: number[]){
         this.x = x;
         this.y = y;
+        this.draw();
     }
 
 
@@ -28,15 +29,31 @@ class Hulknurk {
         this.x.push(x);
         this.y.push(y);
 
-        for(let i = 0; i<this.x.length; i++){
+        for(let i = 0; i < this.x.length - 1; i++){
             ymbermoot+= Math.sqrt(Math.pow(this.x[i + 1] - this.x[i], 2) + Math.pow(this.y[i + 1] - this.y[i], 2));
         }
+        ymbermoot+= Math.sqrt(Math.pow(this.x[0] - this.x[this.x.length - 1], 2) + Math.pow(this.y[0] - this.y[this.y.length - 1], 2));
 
         return ymbermoot;
     }
+
+    draw(): void {
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x[0], this.y[0]);
+        for(let i = 1; i<this.x.length; i++){
+            this.ctx.lineTo(this.x[i], this.y[i]);
+        }
+        this.ctx.lineTo(this.x[0], this.y[0]);
+        this.ctx
+        this.ctx.stroke();
+    }
 }
-
-let hulknurk1 = new Hulknurk([0, 1, 1, 0], [0, 0, 1, 1]);
-let hulknurk2 = new Hulknurk([2, 4, 4, 2], [0, 0, 5, 5]);
-
-hulknurk1.lisaPunkt(3, 4);
+let canvas, ctx, hulknurk1, hulknurk2;
+window.onload = function(){
+    canvas = document.querySelector('#canvas1');
+    ctx = canvas.getContext('2d');
+    hulknurk1 = new Hulknurk(ctx, [100, 200, 200], [100, 100, 200]);
+    hulknurk1.draw();
+    hulknurk1.lisaPunkt(150, 150);
+    hulknurk1.kuva();
+}
